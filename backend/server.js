5d1946629b5f9fefa91d2855c1711c4df48cf7e8
Mohannedkandil <mohanadmkandil@gmail.com>
2022-04-16 02:19:49 +0200

    feat: add routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("ahsadasd");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// Initliaze Express
+
+const app = express();
+
+// Link get goals api gateway with thr route
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
